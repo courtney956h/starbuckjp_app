025eaa968c59ef77c026699215de8af189767bed
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-10-29 15:18:51 +0900

    reward 페이지 연결

--- js/starbucks.js
+++ js/starbucks.js
@@ -28,11 +28,11 @@
     // 헤더박스 스크롤 이벤트시 fixed
     $(window).scroll(function() {
         var sct = $(this).scrollTop()
-        if ( sct>=100 && !$('#header').hasClass('on') ) {
-            $('#header').addClass('on')
+        if ( sct>=95 && !$('#header, #reward_header').hasClass('on') ) {
+            $('#header, #reward_header').addClass('on')
             // $('#header').stop().slideDown(500).addClass('on')
-        } else if ( sct<100 && $('#header').hasClass('on') ) {
-            $('#header').removeClass('on')
+        } else if ( sct<95 && $('#header, #reward_header').hasClass('on') ) {
+            $('#header, #reward_header').removeClass('on')
         }
     })
 
