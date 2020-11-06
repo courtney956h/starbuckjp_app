95abafe7c12d7b7f2377aa4367bd379b0c215e83
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-11-06 14:29:05 +0900

    mobileOrder / storeselect / egift 부분 nav 수정 및 추가

--- js/starbucks.js
+++ js/starbucks.js
@@ -1,7 +1,7 @@
 (function($){
 
     // home 클릭하면 main.html의 #content를 load()
-    $('#wrap').on('click', '#quickMenu .home a, .mobile_btn a, #quickMenu .icon a, .mobileOrder_list .depth1 > a, .prev_btn a, .loginPage_btn a, .joinPage_btn a, .join_btn a', function() {
+    $('#wrap').on('click', '#quickMenu .home a, .mobile_btn a, #quickMenu .icon a, .mobileOrder_list .depth1 > a, .prev_btn a, .loginPage_btn a, .joinPage_btn a, .join_btn a, #quickMenu .pay a', function() {
         var url = this.href;
         $('#container > #content').remove();
         $('#container').load(url + ' #content');
@@ -12,9 +12,28 @@
 
 
     // mobileOrder_nav, storeSelect_nav 클릭시 current 유지
-    $('#wrap').on('click', '.mobileOrder_nav a, .storeSelect_nav a', function() {
-        $('.mobileOrder_nav a.current, .storeSelect_nav a.current').removeClass('current')
+    $('#wrap').on('click', '.storeSelect_nav a', function() {
+        var index = $(this).index();
         $(this).addClass('current')
+        $(this).siblings().removeClass('current')
+        $('.store_select > div').eq(index).addClass('on')
+        $('.store_select > div').eq(index).siblings().removeClass('on')
+    })
+
+    $('#wrap').on('click', '.mobileOrder_nav a', function() {
+        var index = $(this).index();
+        $(this).addClass('current')
+        $(this).siblings().removeClass('current')
+        $('.mobile_order > div').eq(index).addClass('on')
+        $('.mobile_order > div').eq(index).siblings().removeClass('on')
+    })
+
+    $('#wrap').on('click', '.egift_nav a', function() {
+        var index = $(this).index();
+        $(this).addClass('current')
+        $(this).siblings().removeClass('current')
+        $('.egift_order > div').eq(index).addClass('on')
+        $('.egift_order > div').eq(index).siblings().removeClass('on')
     })
 
     // quickMenu 클릭시 current 유지
@@ -23,14 +42,17 @@
         $(this).addClass('current')
     })
 
+
+
+
     // 헤더박스 스크롤 이벤트시 fixed
     $(window).scroll(function() {
         var sct = $(this).scrollTop()
-        if ( sct>=95 && !$('#header, #reward_header, #rewardsbf_header, #mobileOrder_header').hasClass('on') ) {
-            $('#header, #reward_header, #rewardsbf_header, #mobileOrder_header').addClass('on')
+        if ( sct>=95 && !$('#header, #reward_header, #rewardsbf_header').hasClass('on') ) {
+            $('#header, #reward_header, #rewardsbf_header').addClass('on')
             // $('#header').stop().slideDown(500).addClass('on')
-        } else if ( sct<95 && $('#header, #reward_header, #rewardsbf_header, #mobileOrder_header').hasClass('on') ) {
-            $('#header, #reward_header, #rewardsbf_header, #mobileOrder_header').removeClass('on')
+        } else if ( sct<95 && $('#header, #reward_header, #rewardsbf_header').hasClass('on') ) {
+            $('#header, #reward_header, #rewardsbf_header').removeClass('on')
         }
     })
 
